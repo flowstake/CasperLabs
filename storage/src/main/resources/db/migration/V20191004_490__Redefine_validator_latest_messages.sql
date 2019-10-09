a35f063181bf627d8fb6347cdc62d2deba6cc188
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-10-09 10:44:39 +0200

    CON-490: Add `WITHOUT ROWID` to `validator_latest_messages` table definition.

--- storage/src/main/resources/db/migration/V20191004_490__Redefine_validator_latest_messages.sql
+++ storage/src/main/resources/db/migration/V20191004_490__Redefine_validator_latest_messages.sql
@@ -4,5 +4,6 @@ CREATE TABLE validator_latest_messages
 (
     validator  BLOB    NOT NULL,
     block_hash BLOB    NOT NULL,
+    PRIMARY KEY (validator, block_hash),
     FOREIGN KEY (block_hash) REFERENCES block_metadata (block_hash)
-);
+) WITHOUT ROWID;
